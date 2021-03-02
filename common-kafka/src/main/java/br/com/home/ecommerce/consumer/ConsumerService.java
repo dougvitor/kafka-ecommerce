7aba5290b62e2eba48b11dca0566a113bb7c0758
Douglas Vitor <douglas.vitor@gmail.com>
2021-03-02 20:39:07 -0300

    Aula 3.02 - Offset latest e earliest

--- common-kafka/src/main/java/br/com/home/ecommerce/consumer/ConsumerService.java
+++ common-kafka/src/main/java/br/com/home/ecommerce/consumer/ConsumerService.java
@@ -1,6 +1,7 @@
 package br.com.home.ecommerce.consumer;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
@@ -8,7 +9,7 @@ import br.com.home.ecommerce.Message;
 
 public interface ConsumerService<T> {
 	
-	void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
+	void parse(ConsumerRecord<String, Message<T>> record) throws IOException, InterruptedException, ExecutionException;
 	
 	String getTopic();
 	
--- service-email-novo-pedido-compra/src/main/java/br/com/home/ecommerce/EmailNovoPedidoServiceConsumer.java
+++ service-email-novo-pedido-compra/src/main/java/br/com/home/ecommerce/EmailNovoPedidoServiceConsumer.java
@@ -1,31 +1,32 @@
 package br.com.home.ecommerce;
 
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import br.com.home.ecommerce.consumer.KafkaServiceConsumer;
+import br.com.home.ecommerce.consumer.ConsumerService;
+import br.com.home.ecommerce.consumer.ServiceRunner;
 import br.com.home.ecommerce.model.Email;
 import br.com.home.ecommerce.model.PedidoCompra;
 import br.com.home.ecommerce.producer.KafkaServiceProducer;
 
-public class EmailNovoPedidoServiceConsumer {
+public class EmailNovoPedidoServiceConsumer implements ConsumerService<PedidoCompra>{
 	
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-
-		EmailNovoPedidoServiceConsumer emailService = new EmailNovoPedidoServiceConsumer();
-
-		try (var kafkaServiceConsumer = new KafkaServiceConsumer<PedidoCompra>(
-				EmailNovoPedidoServiceConsumer.class.getSimpleName(), 
-				"ECOMMERCE_NEW_ORDER", 
-				emailService::parse,
-				Map.of())) {
-			kafkaServiceConsumer.run();
-		}
+		new ServiceRunner<>(EmailNovoPedidoServiceConsumer::new).start(1);
+	}
+	
+	@Override
+	public String getTopic() {
+		return "ECOMMERCE_NEW_ORDER";
 	}
 
-	private void parse(ConsumerRecord<String, Message<PedidoCompra>> record) throws InterruptedException, ExecutionException {
+	@Override
+	public String getConsumerGroup() {
+		return EmailNovoPedidoServiceConsumer.class.getSimpleName();
+	}
+
+	public void parse(ConsumerRecord<String, Message<PedidoCompra>> record) throws InterruptedException, ExecutionException {
 		try(var kafkaEmailServiceProducer = new KafkaServiceProducer<Email>()){
 			var message = record.value();
 
