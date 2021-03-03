0cb63bc389568cef9d3d07476dcfd59b055bdd60
Douglas Vitor <douglas.vitor@gmail.com>
2021-03-03 11:19:14 -0300

    Aula 5.03 -  Extraindo o common database

--- service-users/src/main/java/br/com/home/ecommerce/CreateUserServiceConsumer.java
+++ service-users/src/main/java/br/com/home/ecommerce/CreateUserServiceConsumer.java
@@ -1,33 +1,26 @@
 package br.com.home.ecommerce;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-
 import br.com.home.ecommerce.consumer.ConsumerService;
 import br.com.home.ecommerce.consumer.ServiceRunner;
+import br.com.home.ecommerce.database.LocalDatabase;
 import br.com.home.ecommerce.model.PedidoCompra;
 
 public class CreateUserServiceConsumer implements ConsumerService<PedidoCompra>{
 	
-	private final Connection connection;
-	
-	private CreateUserServiceConsumer() throws SQLException{
+	private LocalDatabase database;
+
+	public CreateUserServiceConsumer() throws SQLException {
+		database = new LocalDatabase("users_database");
 		
-		String url = "jdbc:sqlite:target/users_database.db";
-		connection = DriverManager.getConnection(url);
-		try {
-			String createTableSQL = "create table Usuario ("
-					+ "uuid varchar(200) primary key,"
-					+ "email varchar(200))";
-			connection.createStatement().execute(createTableSQL);
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
+		String createTableSQL = "create table Usuario ("
+				+ "uuid varchar(200) primary key,"
+				+ "email varchar(200))";
+		
+		database.createIfNotExists(createTableSQL);
 	}
 	
 	public static void main(String[] args) throws SQLException, InterruptedException, ExecutionException {
@@ -61,23 +54,18 @@ public class CreateUserServiceConsumer implements ConsumerService<PedidoCompra>{
 	}
 
 	private boolean isNovoUsuario(String email) throws SQLException {
-		var selectSQL = "select uuid from Usuario"
+		var query = "select uuid from Usuario"
 		+ " where email = ? limit 1";
-		var existsStatement = connection.prepareStatement(selectSQL);
-		existsStatement.setString(1, email);
-		return !existsStatement.executeQuery().next();
+		
+		var results = database.executeQuery(query, email);
+		return !results.next();
 	}
 	
 	private void inseriNovoUsuario(String email) throws SQLException {
-		var insertSQL = "insert into Usuario (uuid, email)"
-				+ "values (?,?)";
+		var preparedStatement = "insert into Usuario (uuid, email) values (?,?)";
+		var uuid = UUID.randomUUID().toString();
 		
-		var insertStatement = connection.prepareStatement(insertSQL);
-		String uuid = UUID.randomUUID().toString();
-		insertStatement.setString(1, uuid);
-		insertStatement.setString(2, email);
-		
-		insertStatement.execute();
+		database.execute(preparedStatement, uuid, email);
 		
 		System.out.println(String.format("Usuario %s e email %s adicionado", uuid, email));
 	
